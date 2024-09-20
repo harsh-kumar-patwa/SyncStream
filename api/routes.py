46d20ec25d9e2b0d8d8a961e248d0dbce2c28781
Harsh Kumar <harshkumar3446@gmail.com>
2024-09-20 18:55:26 +0530

    updated the methods, added send_event

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from database.operations import create_customer,update_customer,delete_customer
+from utils.kafka_client import send_event 
 
 api = Blueprint('api',__name__)
 
@@ -8,22 +9,25 @@ def add_customer():
     data=request.json
     customer_id = create_customer(data['name'],data['email'])
     #After sending the updates to the kafka client we will return
+    send_event('create',{'id':customer_id,'name':data['name'],'email':data['email']})
     return jsonify({'id':customer_id}),201
 
 @api.route('/customers/<int:customer_id>',methods=['PUT'])
 def modify_customer(customer_id):
     data = request.json
-    updated_id = update_customer(customer_id, **data)
-    if updated_id:
+    customer_id_after_update = update_customer(customer_id, **data)
+    if customer_id_after_update:
         #After sending the updates to the kafka client we will return
+        send_event('update',{'id':customer_id_after_update,**data})
         return jsonify({'success': True}), 200
     return jsonify({'success': False, 'message': 'Customer not found'}), 404
 
 @api.route('/customers/<int:customer_id>', methods=['DELETE'])
 def remove_customer(customer_id):
-    deleted_id = delete_customer(customer_id)
-    if deleted_id:
+    id_after_delete = delete_customer(customer_id)
+    if id_after_delete:
          #After sending the updates to the kafka client we will return
+        send_event('delete', {'id': id_after_delete})
         return jsonify({'success': True}), 200
     return jsonify({'success': False, 'message': 'Customer not found'}), 404
 
