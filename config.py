4aa4c2df9260d3b8a31f5fdf3b3478475712e01d
Harsh Kumar <harshkumar3446@gmail.com>
2024-09-20 18:27:39 +0530

    added kafka client methods and updated config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 DATABASE_PATH = 'customer.db'
 STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
+KAFKA_SERVER = os.getenv('KAFKA_SERVER', 'localhost:9092')
+KAFKA_TOPIC = 'customer_events'
--- utils/kafka_client.py
+++ utils/kafka_client.py
@@ -12,3 +12,10 @@ consumer = KafkaConsumer(
     bootstrap_servers=[KAFKA_SERVER],
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
+
+def send_event(event_type, data):
+    producer.send(KAFKA_TOPIC, {'type': event_type, 'data': data})
+    producer.flush()
+
+def get_consumer():
+    return consumer
