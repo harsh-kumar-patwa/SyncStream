94b45c6e18fe3f4fd344267867a86ff5f565268d
Harsh Kumar <harshkumar3446@gmail.com>
2024-09-20 22:13:37 +0530

    changes in delete

--- sync/outsync.py
+++ sync/outsync.py
@@ -7,11 +7,12 @@ stripe_integration = StripeIntegration()
 def send_update_to_external_system(event):
     if event['type']=='create':
         stripe_id = stripe_integration.create_customer(event['data']['name'], event['data']['email'])
+        print(stripe_id+"\n")
         update_customer(event['data']['id'],stripe_id=stripe_id)
     elif event['type']=='update':
         stripe_integration.update_customer(event['data']['stripe_id'], **event['data'])
     elif event['type']=='delete':
-        stripe_integration.delete_customer(event['data']['stripe_id'])
+        stripe_integration.delete_customer(event['data']['id'])
 
 def start_outsync():
     consumer= get_consumer();
